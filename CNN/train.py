90e3e23cce6ffe43d616c6db16219c0325686dc5
Mahmudulla Hassan <mhassan@miners.utep.edu>
2018-02-28 01:46:14 -0700

    New network architecture tuned to run without memory having memory issue. Multi-layer model is used to share the workload.
    Former-commit-id: b111782feae9ab78e9876f69cfa3c0abc130470c

--- CNN/train.py
+++ CNN/train.py
@@ -5,7 +5,7 @@
 # Required files: data_std.pickle, refined_set_dataset.csv    #
 #                                                             #
 # Written by: Mahmudulla Hassan                               #
-# Last Modified: 02/18/2018                                   #
+# Last Modified: 02/27/2018                                   #
 ###############################################################
 
 import pickle
@@ -44,26 +44,29 @@ def read_score():
 
     return score
 
+# Batch generator for training
 def generator(data, batch_size):
+    if len(data) < batch_size:
+        batch_size = len(data)
+    
     score = read_score()
     rand_keys = np.random.choice(list(data.keys()), batch_size)
-    x = np.empty([batch_size, 165, 285, 250, 8])
-    x[0, :, :, :, :] = data[rand_keys[0]]
-    y = np.empty(batch_size)
-    y[0] = np.float32(score[rand_keys[0]])
+    data_dim = list(data[list(data.keys())[0]].shape)
     
     while(True):
+        x = np.empty([batch_size] + data_dim)
+        x[0, :, :, :, :] = data[rand_keys[0]]
+        y = np.empty(batch_size)
+        y[0] = np.float32(score[rand_keys[0]])
+
         for i in range(1, len(rand_keys)):
             x[i, :, :, :, :] = data[rand_keys[i]]
             y[i] = score[rand_keys[i]]
-    
-        yield (x, y)
 
+        yield x, y
+
+# Model generator
 def get_model(summary=False):
-    """ Return the Keras model of the network
-    """
-    model = Sequential()
-    # 1st layer group
     # input_shape = (batch, spatial_dim1, spatial_dim2, spatial_dim3, channels)
 #     model.add(Convolution3D(filters,
 #                             kernel_size,
@@ -83,93 +86,84 @@ def get_model(summary=False):
 #                            )
 #              )
 
+    model = Sequential()
+    
+    # 1st layer group
     model.add(Conv3D(filters=64,
                      kernel_size=(5, 5, 5),
+                     strides = (2, 2, 2),
                      input_shape=(165, 285, 250, 8),
-                     padding='same',
-                     dilation_rate=(1, 1, 1),
+                     padding='valid',
                      activation='relu',
                      kernel_initializer='glorot_uniform',
                      bias_initializer='zeros'))
     
-    model.add(MaxPooling3D(pool_size=(2, 2, 2), 
-                           strides=(2, 2, 2), 
+    model.add(MaxPooling3D(pool_size=(2, 2, 2),
                            padding='valid'))
     
     # 2nd layer group
-#    model.add(Conv3D(filters=128,
-#                     kernel_size=(3, 3, 3),
-#                     activation='relu',
-#                     padding='same',
-#                     dilation_rate=(1, 1, 1)))
+    model.add(Conv3D(filters=128,
+                     kernel_size=(3, 3, 3),
+                     activation='relu',
+                     padding='valid'))
     
-#    model.add(MaxPooling3D(pool_size=(2, 2, 2), 
-#                           strides=(2, 2, 2), 
-#                           padding='valid'))
-#     # 3rd layer group
-#     model.add(Convolution3D(256, 3, 3, 3, activation='relu', 
-#                             border_mode='same', name='conv3a',
-#                             subsample=(1, 1, 1)))
-#     model.add(Convolution3D(256, 3, 3, 3, activation='relu', 
-#                             border_mode='same', name='conv3b',
-#                             subsample=(1, 1, 1)))
-#     model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(1, 1, 1), 
-#                            border_mode='valid', name='pool3'))
-#     # 4th layer group
-#     model.add(Convolution3D(512, 3, 3, 3, activation='relu', 
-#                             border_mode='same', name='conv4a',
-#                             subsample=(1, 1, 1)))
-#     model.add(Convolution3D(512, 3, 3, 3, activation='relu', 
-#                             border_mode='same', name='conv4b',
-#                             subsample=(1, 1, 1)))
-#     model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(1, 1, 1), 
-#                            border_mode='valid', name='pool4'))
-    # 5th layer group
+    model.add(MaxPooling3D(pool_size=(2, 2, 2),
+                           padding='valid'))
+    
+    # 3rd layer group
+    model.add(Conv3D(filters=256,
+                     kernel_size=(3, 3, 3),
+                     activation='relu',
+                     padding='valid'))
+    
+    model.add(MaxPooling3D(pool_size=(2, 2, 2),
+                           strides=(1, 1, 1),
+                           padding='valid'))
+    
+    # 4th layer group
     model.add(Conv3D(filters=512,
                      kernel_size=(3, 3, 3),
                      activation='relu',
-                     padding='same',
-                     dilation_rate=(1, 1, 1)))
+                     padding='valid'))
     
-#    model.add(Conv3D(filters=512,
-#                     kernel_size=(3, 3, 3),
-#                     activation='relu', 
-#                     padding='same',
-#                     dilation_rate=(1, 1, 1)))
-    
-    model.add(ZeroPadding3D(padding=(0, 1, 1)))
-    model.add(MaxPooling3D(pool_size=(2, 2, 2), 
-                           strides=(2, 2, 2), 
+    model.add(MaxPooling3D(pool_size=(2, 2, 2),
+                           strides=(1, 1, 1),
                            padding='valid'))
+    
+    # Reduce the number of parameters (!!!)
+    model.add(AveragePooling3D(pool_size=(2, 2, 2), 
+                               padding='valid'))
+                           
     model.add(Flatten())
-    # FC layers group
-    #model.add(Dense(1024, activation='relu'))
-    #model.add(Dropout(.5))
-    model.add(Dense(512, activation='relu'))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(.5))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(.5))
+    model.add(Dense(32, activation='relu'))
     model.add(Dropout(.5))
     model.add(Dense(1, activation='linear'))
     if summary:
         print(model.summary())
     return model
 
-# Create the model
-model = get_model()
-# Print the model summary
-model.summary()
-# Compile the model
+
+
+# Set dynamic memory allocation in a specific gpu
+# K.clear_session()
+config = K.tf.ConfigProto()
+config.gpu_options.allow_growth = True
+# config.gpu_options.visible_device_list = "0"
+K.set_session(K.tf.Session(config=config))
+
+model = get_model(summary=True)
+model = multi_gpu_model(model, gpus=4)
 model.compile(optimizer='adam',
               loss='mean_squared_error',
               metrics=[metrics.mse])
 
-# history = model.fit(x=, y=, batch_size=5, epochs=10, validation_split=0.2, verbose=1 , use_multiprocessing=True, )
 
-# Train the model
-history = model.fit_generator(generator=generator(data=data, batch_size=5),
-                              epochs=5,
-                              steps_per_epoch=6,
-                              validation_data=generator(data=data, batch_size=1),
-                              validation_steps=2,
-                              verbose=1, 
-                              workers=mp.cpu_count())#,
-#                              use_multiprocessing=True)
-
+batch_size = 5
+history = model.fit_generator(generator=generator(data=data, batch_size=batch_size),
+                             epochs = 20,
+                             steps_per_epoch=len(data)//batch_size,
+                             verbose = 1)
