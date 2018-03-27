8ccd7ac6762da3b8fccb13a2b6a3babfa55ef690
Mahmudulla Hassan <mhassan@miners.utep.edu>
2018-03-27 14:37:39 -0600

    Required files are organized. Extra files are now inside dl_networks_02 directory.
    Former-commit-id: 1cad08a2734e040d9b11d084fa460eba9f3fdf3c

--- test_run.sh
+++ test_run.sh
@@ -1,1 +1,1 @@
-python NNScore2.01.04.py -receptor samples/10gs/10gs_protein.pdbqt -ligand samples/10gs/10gs_ligand.pdbqt -vina_executable  /opt/autodock_vina_1_1_2_linux_x86/bin/vina
+python NNScore2.01.02.py -receptor samples/10gs/10gs_protein.pdbqt -ligand samples/10gs/10gs_ligand.pdbqt -vina_executable  /opt/autodock_vina_1_1_2_linux_x86/bin/vina
