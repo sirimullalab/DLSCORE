054cf0ad805d40401737f3c820c249cc71699fbc
Md Mahmudulla Hassan <tg835677@login3.stampede2.tacc.utexas.edu>
2018-04-06 18:40:50 -0500

    Modify the test script file

--- test_run.sh
+++ test_run.sh
@@ -1,1 +1,2 @@
-python NNScore2.01.02.py -receptor samples/10gs/10gs_protein.pdbqt -ligand samples/10gs/10gs_ligand.pdbqt -vina_executable  /opt/autodock_vina_1_1_2_linux_x86/bin/vina
+python dlscore2.py -r samples/10gs/10gs_protein.pdb -l samples/10gs/10gs_ligand.pdbqt -v /work/04268/tg835677/stampede2/autodock_vina_1_1_2_linux_x86/bin/vina -n 10
+#python NNScore2.01.02.py -receptor samples/10gs/10gs_protein.pdbqt -ligand samples/10gs/10gs_ligand.pdbqt -vina_executable  /opt/autodock_vina_1_1_2_linux_x86/bin/vina
