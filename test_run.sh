29540692b62d5044e66445a31efbb819010819b9
Mahmudulla Hassan <mhassan@miners.utep.edu>
2020-05-10 17:39:33 -0600

    Update test_run.sh
    Former-commit-id: 207dbfa15b19f6b077eb25d9a18086151f9b8d2c

--- test_run.sh
+++ test_run.sh
@@ -2,4 +2,4 @@
 #python dlscore.py --receptor samples/10gs/10gs_protein.pdb --ligand samples/10gs/10gs_ligand.pdbqt --network_type general --verbose 1
 
 # Multiple ligands
-python dlscore.py --receptor samples/ampc/receptor.pdbqt --ligand samples/ampc/ligand.pdbqt --network_type general --verbose 1
+python dlscore.py --receptor samples/ampc/receptor.pdbqt --ligand samples/ampc/ligand.pdbqt --network_type refined --verbose 1
