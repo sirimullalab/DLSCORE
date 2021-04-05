9fd591417a3b1579cf82a28d9b846d10d008bc8d
Mahmudulla Hassan <mhassan@miners.utep.edu>
2021-04-05 17:52:38 -0600

    fix broken links
    Former-commit-id: 8b7d856b32ccac2c4dfd3c7d01499c0591b572f4

--- setup.sh
+++ setup.sh
@@ -8,8 +8,8 @@ pip install -r requirements.txt
 
 if [ ! -d ./networks ]; then
   mkdir networks/
-  curl -O https://drugdiscovery.utep.edu/files/dlscore/general.tar.gz
-  curl -O https://drugdiscovery.utep.edu/files/dlscore/refined.tar.gz
+  curl -o general.tar.gz https://drugdiscovery.utep.edu/download.php?file=general.tar.gz
+  curl -o refined.tar.gz https://drugdiscovery.utep.edu/download.php?file=refined.tar.gz
 
   tar -xvf general.tar.gz && mv general networks/ && rm -f general.tar.gz
   tar -xvf refined.tar.gz && mv refined networks/ && rm -f refined.tar.gz
