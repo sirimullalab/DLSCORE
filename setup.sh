4017e851186087b1d4622e8d0d13d8e904288014
hassanmohsin <mhassan@miners.utep.edu>
2021-04-08 03:26:26 -0600

    download only 10 network weights

--- setup.sh
+++ setup.sh
@@ -8,9 +8,13 @@ pip install -r requirements.txt
 
 if [ ! -d ./networks ]; then
   mkdir networks/
-  curl -o general.tar.gz https://drugdiscovery.utep.edu/download.php?file=general.tar.gz
-  curl -o refined.tar.gz https://drugdiscovery.utep.edu/download.php?file=refined.tar.gz
-
+  # Download top 10 network weights
+  curl -o general.tar.gz https://drugdiscovery.utep.edu/download.php?file=general-10.tar.gz
+  curl -o refined.tar.gz https://drugdiscovery.utep.edu/download.php?file=refined-10.tar.gz
+  
+  # Download all 100 network weights
+  # curl -o general.tar.gz https://drugdiscovery.utep.edu/download.php?file=general-10.tar.gz
+  # curl -o refined.tar.gz https://drugdiscovery.utep.edu/download.php?file=refined-10.tar.gz
   tar -xvf general.tar.gz && mv general networks/ && rm -f general.tar.gz
   tar -xvf refined.tar.gz && mv refined networks/ && rm -f refined.tar.gz
 fi
